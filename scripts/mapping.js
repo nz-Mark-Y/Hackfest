210dde84d8d44b82ee63c2ee4243906d30d66ef8
Irsan Arisandy <irsanarisandy@hotmail.com>
2016-08-13 14:14:02 +1200

    Fix indentation

--- scripts/mapping.js
+++ scripts/mapping.js
@@ -90,69 +90,68 @@ function init() {
 				iw.close();
 				infoWindowVisible(false);
 			} else {
-				//var html = "<div style='color:#000;background-color:#fff;padding:5px;width:150px;'><h4>" + title + "</h4><p>" + desc + "<p><p>" + telephone + "<p><a href='" + link + "'' >" + web + "<a></div>";
+				// styling of marker text (fix when have time)
+				var html = "<div style='color:#000;background-color:#fff;padding:5px;width:150px;'><h4>" + title + "</h4><p>" + desc + "<p><p>" + telephone + "<p><a href='" + link + "'' >" + web + "<a></div>";
+
 				iw = new google.maps.InfoWindow({
 					content: html
 				});
-			iw.open(map, marker);
-			infoWindowVisible(true);
+				iw.open(map, marker);
+				infoWindowVisible(true);
 			}
 		});
 		google.maps.event.addListener(iw, 'closeclick', function() {
-		  infoWindowVisible(false);
+			infoWindowVisible(false);
 		});
-		}
+	}
 
-		  // Create the search box and link it to the UI element.
-		var input = /** @type {HTMLInputElement} */(
-		  document.getElementById('pac-input'));
+	// Create the search box and link it to the UI element.
+	var input = /** @type {HTMLInputElement} */(
+		document.getElementById('pac-input'));
 		map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
 		var searchBox = new google.maps.places.SearchBox(
 		/** @type {HTMLInputElement} */(input));
 
-		// Listen for the event fired when the user selects an item from the
-		// pick list. Retrieve the matching places for that item.
+		// Listen for the event fired when the user selects an item from the pick list. Retrieve the matching places for that item.
 		google.maps.event.addListener(searchBox, 'places_changed', function() {
-		var places = searchBox.getPlaces();
+			var places = searchBox.getPlaces();
 
-		if (places.length == 0) {
-		  return;
-		}
-		for (var i = 0, marker; marker = markers[i]; i++) {
-		  marker.setMap(null);
-		}
+			if (places.length == 0) {
+				return;
+			}
+			for (var i = 0, marker; marker = markers[i]; i++) {
+				marker.setMap(null);
+			}
 
-		// For each place, get the icon, place name, and location.
-		markers = [];
-		var bounds = new google.maps.LatLngBounds();
-		for (var i = 0, place; place = places[i]; i++) {
-		  var image = {
-		    url: place.icon,
-		    size: new google.maps.Size(71, 71),
-		    origin: new google.maps.Point(0, 0),
-		    anchor: new google.maps.Point(17, 34),
-		    scaledSize: new google.maps.Size(25, 25)
-		  };
+			// For each place, get the icon, place name, and location.
+			markers = [];
+			var bounds = new google.maps.LatLngBounds();
+			for (var i = 0, place; place = places[i]; i++) {
+				var image = {
+					url: place.icon,
+					size: new google.maps.Size(71, 71),
+					origin: new google.maps.Point(0, 0),
+					anchor: new google.maps.Point(17, 34),
+					scaledSize: new google.maps.Size(25, 25)
+				};
 
-		  // Create a marker for each place.
-		  var marker = new google.maps.Marker({
-		    map: map,
-		    icon: image,
-		    title: place.name,
-		    position: place.geometry.location
-		  });
+				// Create a marker for each place.
+				var marker = new google.maps.Marker({
+					map: map,
+					icon: image,
+					title: place.name,
+					position: place.geometry.location
+				});
 
-		  markers.push(marker);
+				markers.push(marker);
 
-		  bounds.extend(place.geometry.location);
-		}
-
-		map.fitBounds(bounds);
+				bounds.extend(place.geometry.location);
+			}
+			map.fitBounds(bounds);
 		});
 
-		// Bias the SearchBox results towards places that are within the bounds of the
-		// current map's viewport.
+		// Bias the SearchBox results towards places that are within the bounds of the current map's viewport.
 		google.maps.event.addListener(map, 'bounds_changed', function() {
 		var bounds = map.getBounds();
 		searchBox.setBounds(bounds);
