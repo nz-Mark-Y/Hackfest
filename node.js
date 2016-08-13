dd8995391c9a46f3195f59bea21b82eff5967357
Karen Panoedjoe <kpanoedjoe@users.noreply.github.com>
2016-08-13 16:15:52 +1200

    Update node.js

--- node.js
+++ node.js
@@ -14,31 +14,30 @@ app.get('/getdealsforlocation', function (req, res) {
       public: 'vfZcastyjgwkfXaDKtczSLY8JVBW943B',
       secret: 'F8-P5ZsXTkhEH_brQfVdeZAPLDs'
   });
-  function filterData(data) {
-    //console.log('data', data);
-    var filteredData = [];
-    var businesses = data.businesses;
-    businesses.map(function(business){
-        var newObject = {};
-        newObject.name = business.name;
-        newObject.deals = business.deals;
-        newObject.address = business.location.display_address;
-        newObject.coordinates = business.location.coordinate
-        filteredData.push(newObject);
-    });
-    return filteredData;
 
-  }
 //list user timeline
   twitter.get('https://api.yelp.com/v2/search?cll='+ req.query.lat + ',' + req.query.lon +'&location=' + req.query.location + '&deals_filter=true&term=food&radius_filter='+req.query.radius_filter, function(err, thing, data) {
     res.setHeader('Content-Type','application/json');
-    //console.log('thing', data.businesses);
-    //console.log('total', data.total);
-    //console.log(thing);
       var filteredData = filterData(JSON.parse(data))//(data);
       res.send(filteredData);//data);
   });
 });
+
+function filterData(data) {
+  //console.log('data', data);
+  var filteredData = [];
+  var businesses = data.businesses;
+  businesses.map(function(business){
+      var newObject = {};
+      newObject.name = business.name;
+      newObject.deals = business.deals;
+      newObject.address = business.location.display_address;
+      newObject.coordinates = business.location.coordinate
+      filteredData.push(newObject);
+  });
+  return filteredData;
+
+}
 
 
 
