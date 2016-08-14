94379eef36bd55e4da7f9dc4c79d175515b1f827
Mark Yep <ooksei@outlook.com>
2016-08-14 20:21:20 +1200

    nvm lol

--- node.js
+++ node.js
@@ -5,7 +5,6 @@ app.set('port', (process.env.PORT || 3000));// checks for environment variable p
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
