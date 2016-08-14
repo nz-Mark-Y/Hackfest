a697e50cf258a08b83073b971c3e8d53dc65e6ef
Irsan Arisandy <irsanarisandy@hotmail.com>
2016-08-14 13:47:10 +1200

    fix

--- scripts/direction.js
+++ scripts/direction.js
@@ -1,10 +1,11 @@
+var tempPosition;
+
 function action1() {
-	//console.log(document.getElementById('end').value);
-	if (yolo === undefined) sendRequest();
 	placingMarker();
 }
 
 function action2() {
+	if (yolo === undefined) sendRequest();
 	if (yolo.readyState == 4) {
 		calculateAndDisplayRoute();
 	}
@@ -59,43 +60,32 @@ function placingMarker() {
 				});
 				iw.open(map, marker);
 				infoWindowVisible(true);
+				tempPosition = [marker.position.lat(), marker.position.lng()];
 			}
 		});
 	}
 }
 
 function calculateAndDisplayRoute() {
+	var position = new google.maps.LatLng(tempPosition[0], tempPosition[1]);
     // First, remove any existing markers from the map
 	if (markers.length !== 0) {
 		for (i = 0; i < markers.length; i++) markers[i].setMap(null);
 	}
-	console.log(markers[0]);
 
 	// Retrieve the start and end locations and create a DirectionsRequest using travelMode value
 	// travelMode can be DRIVING, BICYLING, TRANSIT, WALKING
 	directionsService.route({
 		origin: input.value,
-		destination: "Madison Av/E 58 St, New York, NY, United States",
+		destination: position,
 		travelMode: 'WALKING'
 	}, function(response, status) {
 		// Route the directions and pass the response to a function to create
 		// markers for each step.
 		if (status === 'OK') {
 			directionsDisplay.setDirections(response);
-			showSteps(response);
 		} else {
 			window.alert('Directions request failed due to ' + status);
 		}
 	});
-}
-
-function showSteps(directionResult) {
-	// For each step, place a marker, and add the text to the marker's infowindow.
-	// Also attach the marker to an array so we can keep track of it and remove it when calculating new routes.
-	var myRoute = directionResult.routes[0].legs[0];
-	for (var i = 0; i < myRoute.steps.length; i++) {
-		var marker = markers[i] = markers[i] || new google.maps.Marker();
-		marker.setMap(map);
-		marker.setPosition(myRoute.steps[i].start_location);
-	}
 }
