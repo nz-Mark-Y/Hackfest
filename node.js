95d44b4febe3fd72cec5f0cc3390e34caf6c35a6
Mark Yep <ooksei@outlook.com>
2016-08-14 20:16:54 +1200

    CORS

--- node.js
+++ node.js
@@ -5,6 +5,7 @@ app.set('port', (process.env.PORT || 3000));// checks for environment variable p
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
